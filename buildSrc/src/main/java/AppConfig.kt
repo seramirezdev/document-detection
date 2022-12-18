2ee6ba910f69b401ce722c1b202e957a730fd5ac
Sergio Ramírez <seramirezdev@gmail.com>
2022-12-18 00:26:45 -0500

    Example project created

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,7 +1,10 @@
-object LibConfig {
+object AppConfig {
     const val compileSdk = 32
     const val targetSdk = 32
     const val minSdk = 21
+
+    const val versionCode = 1
+    const val versionName = "1.0"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 }
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,4 +1,8 @@
 object Dependencies {
 
     const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
 }
